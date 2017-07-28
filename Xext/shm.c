268c56c197b2cba46347e85312b601250d93f969
Michal Srb <msrb@suse.com>
2017-07-28 16:27:10 +0200

    Xext/shm: Validate shmseg resource id (CVE-2017-13721)
    Otherwise it can belong to a non-existing client and abort X server with
    FatalError "client not in use", or overwrite existing segment of another
    existing client.
    
    Signed-off-by: Julien Cristau <jcristau@debian.org>
    (cherry picked from commit b95f25af141d33a65f6f821ea9c003f66a01e1f1)
    (cherry picked from commit a510fb811100bc27f0bfafe5d073998551161819)

--- Xext/shm.c
+++ Xext/shm.c
@@ -1215,6 +1215,7 @@ ProcShmCreateSegment(ClientPtr client)
     };
 
     REQUEST_SIZE_MATCH(xShmCreateSegmentReq);
+    LEGAL_NEW_RESOURCE(stuff->shmseg, client);
     if ((stuff->readOnly != xTrue) && (stuff->readOnly != xFalse)) {
         client->errorValue = stuff->readOnly;
         return BadValue;
